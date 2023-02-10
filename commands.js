792b071daad3e1561b46c47a0f250cfe5fb0801c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-10 15:15:25 -0500

    feat: serialize dom objects (#15)
    * feat: log jQuery elements better
    
    * feat: good jQuery serialization:

--- commands.js
+++ commands.js
@@ -9,20 +9,74 @@ const stringify = require('safe-stable-stringify')
 
 function formatTitle(pattern, x) {
   if (pattern.includes('{}') || pattern.includes('{0}')) {
-    x = stringify(x)
+    x = stringifyObjectOrJquery(x)
   }
   if (pattern.includes('%d')) {
     return pattern.replace('%d', x)
   }
   if (pattern.includes('%o')) {
-    return pattern.replace('%o', stringify(x))
+    return pattern.replace('%o', stringifyObjectOrJquery(x))
   }
   return format(pattern, x)
 }
 
+function stringifyDomAttributes(el) {
+  if (!el.attributes.length) {
+    return ''
+  }
+  return Cypress._.map(el.attributes, (a) => {
+    if (a.nodeName === 'id' || a.nodeName === 'class') {
+      return
+    }
+    return a.nodeName + '=' + a.nodeValue
+  })
+    .filter(Boolean)
+    .join(' ')
+}
+
+function stringifyDomElement(el) {
+  const attrs = stringifyDomAttributes(el)
+  return `<${el.tagName.toLowerCase()}${el.id ? '#' + el.id : ''}${
+    el.className ? '.' + el.className : ''
+  }${attrs ? ' ' + attrs : ''}/>`
+}
+
+function stringifyjQuery(subject) {
+  let s = `$ of ${subject.length}`
+  if (subject.length) {
+    const elements = subject.toArray()
+    if (subject.length > 3) {
+      // skip the middle elements
+      const els =
+        '[' +
+        stringifyDomElement(elements[0]) +
+        '...' +
+        stringifyDomElement(elements.at(-1)) +
+        ']'
+      s += ' ' + els
+    } else {
+      const els = elements.map(stringifyDomElement).join(',')
+      s += ' ' + els
+    }
+  }
+  const maxLength = chai.config.truncateThreshold || 200
+  if (s.length > maxLength) {
+    return s.slice(0, maxLength) + '...'
+  }
+  return s
+}
+
+function stringifyObjectOrJquery(subject) {
+  if (Cypress.dom.isJquery(subject)) {
+    return stringifyjQuery(subject)
+  } else {
+    return stringify(subject)
+  }
+}
+
 Cypress.Commands.overwrite('log', (logCommand, formatPattern, ...args) => {
   const log = Cypress.log({ name: 'log', message: '' })
-  const subject = Cypress.state('subject')
+  let subject = Cypress.state('subject')
 
   let formatted = formatPattern
   if (typeof formatPattern === 'string') {
@@ -30,10 +84,10 @@ Cypress.Commands.overwrite('log', (logCommand, formatPattern, ...args) => {
   } else if (typeof formatPattern === 'function') {
     formatted = formatPattern(subject)
     if (typeof formatted !== 'string') {
-      formatted = stringify(formatted)
+      formatted = stringifyObjectOrJquery(formatted)
     }
   } else {
-    formatted = stringify(subject)
+    formatted = stringifyObjectOrJquery(subject)
   }
 
   log.set('message', formatted)
