ee5f643e6662e06e9ffcd1a23ba967de803b0139
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-10 13:56:20 -0500

    fix: handle circular objects

---
+++ cypress/e2e/circular.cy.js
@@ -0,0 +1,19 @@
+/// <reference path="../../src/index.d.ts" />
+
+import '../../commands'
+
+it('logs circular object', () => {
+  const obj = {
+    name: 'object',
+  }
+  obj.next = obj
+  // try to log a circular object
+  cy.wrap(obj, { log: false })
+    .log()
+    .log('with format string')
+    .log('circular %o')
+})
+
+it('logs jQuery subject', () => {
+  cy.wrap(Cypress.$('<div id="obj" class="my-object">Hello</div>')).log()
+})
