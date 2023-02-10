549978d3b28ca60df56abca1acf11525d2f16901
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-10 18:16:27 -0500

    move util to src

--- commands.js
+++ commands.js
@@ -1,87 +1,20 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
+// @ts-check
 
-// to avoid relying on old polyfills for node "format"
-// use a custom formatter plus our own code
-const format = require('string-format')
-const stringify = require('safe-stable-stringify')
-
-function formatTitle(pattern, x) {
-  if (pattern.includes('{}') || pattern.includes('{0}')) {
-    x = stringifyObjectOrJquery(x)
-  }
-  if (pattern.includes('%d')) {
-    return pattern.replace('%d', x)
-  }
-  if (pattern.includes('%o')) {
-    return pattern.replace('%o', stringifyObjectOrJquery(x))
-  }
-  return format(pattern, x)
-}
-
-function stringifyDomAttributes(el) {
-  if (!el.attributes.length) {
-    return ''
-  }
-  return Cypress._.map(el.attributes, (a) => {
-    if (a.nodeName === 'id' || a.nodeName === 'class') {
-      return
-    }
-    return a.nodeName + '=' + a.nodeValue
-  })
-    .filter(Boolean)
-    .join(' ')
-}
-
-function stringifyDomElement(el) {
-  const attrs = stringifyDomAttributes(el)
-  return `<${el.tagName.toLowerCase()}${el.id ? '#' + el.id : ''}${
-    el.className ? '.' + el.className : ''
-  }${attrs ? ' ' + attrs : ''}/>`
-}
-
-function stringifyjQuery(subject) {
-  let s = `$ of ${subject.length}`
-  if (subject.length) {
-    const elements = subject.toArray()
-    if (subject.length > 3) {
-      // skip the middle elements
-      const els =
-        '[' +
-        stringifyDomElement(elements[0]) +
-        '...' +
-        stringifyDomElement(elements.at(-1)) +
-        ']'
-      s += ' ' + els
-    } else {
-      const els = elements.map(stringifyDomElement).join(',')
-      s += ' ' + els
-    }
-  }
-  const maxLength = chai.config.truncateThreshold || 200
-  if (s.length > maxLength) {
-    return s.slice(0, maxLength) + '...'
-  }
-  return s
-}
-
-function stringifyObjectOrJquery(subject) {
-  if (Cypress.dom.isJquery(subject)) {
-    return stringifyjQuery(subject)
-  } else {
-    return stringify(subject)
-  }
-}
+const { formatTitle, stringifyObjectOrJquery } = require('./src/utils')
 
 Cypress.Commands.overwrite('log', (logCommand, formatPattern, ...args) => {
   const log = Cypress.log({ name: 'log', message: '' })
+  // @ts-ignore
   let subject = Cypress.state('subject')
 
   let formatted = formatPattern
   if (typeof formatPattern === 'string') {
     formatted = formatTitle(formatPattern, subject)
   } else if (typeof formatPattern === 'function') {
+    // @ts-ignore
     formatted = formatPattern(subject)
     if (typeof formatted !== 'string') {
       formatted = stringifyObjectOrJquery(formatted)
