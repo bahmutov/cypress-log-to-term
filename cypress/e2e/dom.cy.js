670e7aa4e4d1dbc1f5ba743864d2b59d6d70f15e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-13 09:57:38 -0500

    feat: print the short and long text better (#19)
    * feat: print the short and long text better
    
    * include text in the single argument with format string

--- cypress/e2e/dom.cy.js
+++ cypress/e2e/dom.cy.js
@@ -1,15 +1,19 @@
 /// <reference path="../../src/index.d.ts" />
 
 import '../../commands'
-import { stringifyObjectOrJquery } from '../../src/utils'
+import { formatTitle, stringifyObjectOrJquery } from '../../src/utils'
 
 chai.config.truncateThreshold = 200
 
 describe('jQuery objects', () => {
   it('stringifies jQuery', () => {
     const $el = Cypress.$('<div id="obj" class="my-object">Hello</div>')
-    const s = stringifyObjectOrJquery($el)
-    expect(s, 'div').to.equal('$ of 1 <div#obj.my-object />')
+    const o = stringifyObjectOrJquery($el)
+    expect(o).to.be.an('object').and.have.keys(['short', 'full'])
+    expect(o.short, 'short text').to.equal('$ of 1 <div#obj.my-object />')
+    expect(o.full, 'full text').to.equal(
+      '$ of 1 <div#obj.my-object>Hello</div>',
+    )
   })
 
   it('logs jQuery subject', () => {
@@ -21,6 +25,21 @@ describe('jQuery objects', () => {
     cy.visit('cypress/index.html')
     cy.get('h1').log()
     cy.get('p').log()
+  })
+
+  it('formatTitle with DOM element', () => {
+    const $el = Cypress.$('<div id="obj" class="my-object">Hello</div>')
+    const o = formatTitle('hello says %o', $el)
+    expect(o).to.have.keys(['short', 'full'])
+    expect(o).to.deep.equal({
+      short: 'hello says $ of 1 <div#obj.my-object />',
+      full: 'hello says $ of 1 <div#obj.my-object>Hello</div>',
+    })
+  })
+
+  it('logs one DOM element with text with format string', () => {
+    cy.visit('cypress/index.html')
+    cy.get('h1').log('h1 is %o')
   })
 
   it('logs multiple elements', () => {
--- src/utils.js
+++ src/utils.js
@@ -4,14 +4,28 @@ const format = require('string-format')
 const stringify = require('safe-stable-stringify')
 
 function formatTitle(pattern, x) {
+  if (typeof x === 'string') {
+    x = {
+      short: x,
+      full: x,
+    }
+  }
+
   if (pattern.includes('{}') || pattern.includes('{0}')) {
     x = stringifyObjectOrJquery(x)
   }
   if (pattern.includes('%d')) {
-    return pattern.replace('%d', x)
+    return {
+      short: pattern.replace('%d', x.short),
+      full: pattern.replace('%d', x.full),
+    }
   }
   if (pattern.includes('%o')) {
-    return pattern.replace('%o', stringifyObjectOrJquery(x))
+    x = stringifyObjectOrJquery(x)
+    return {
+      short: pattern.replace('%o', x.short),
+      full: pattern.replace('%o', x.full),
+    }
   }
   return format(pattern, x)
 }
@@ -35,6 +49,21 @@ function stringifyDomElement(el) {
   return `<${el.tagName.toLowerCase()}${el.id ? '#' + el.id : ''}${
     el.className ? '.' + el.className : ''
   }${attrs ? ' ' + attrs : ''} />`
+}
+
+function stringifyDomElementWithText(el) {
+  const attrs = stringifyDomAttributes(el)
+  const text = el.innerText
+  const tag = el.tagName.toLowerCase()
+  if (text.length < 30) {
+    return `<${tag}${el.id ? '#' + el.id : ''}${
+      el.className ? '.' + el.className : ''
+    }${attrs ? ' ' + attrs : ''}>${text}</${tag}>`
+  } else {
+    return `<${el.tagName.toLowerCase()}${el.id ? '#' + el.id : ''}${
+      el.className ? '.' + el.className : ''
+    }${attrs ? ' ' + attrs : ''} />`
+  }
 }
 
 function stringifyjQuery(subject) {
@@ -62,12 +91,39 @@ function stringifyjQuery(subject) {
   return s
 }
 
+function stringifyjQueryWithText(subject) {
+  let s = `$ of ${subject.length}`
+  if (subject.length) {
+    const elements = subject.toArray()
+    if (subject.length > 3) {
+      // skip the middle elements
+      const els =
+        '[' +
+        stringifyDomElementWithText(elements[0]) +
+        '...' +
+        stringifyDomElementWithText(elements.at(-1)) +
+        ']'
+      s += ' ' + els
+    } else {
+      const els = elements.map(stringifyDomElementWithText).join(',')
+      s += ' ' + els
+    }
+  }
+  const maxLength = 2000
+  if (s.length > maxLength) {
+    return s.slice(0, maxLength) + '...'
+  }
+  return s
+}
+
 function stringifyObjectOrJquery(subject) {
-  debugger
   if (Cypress.dom.isJquery(subject)) {
-    return stringifyjQuery(subject)
+    return {
+      short: stringifyjQuery(subject),
+      full: stringifyjQueryWithText(subject),
+    }
   } else {
-    return stringify(subject)
+    return { short: stringify(subject) }
   }
 }
 
